2d8fa70e2daf89e7e522b86b48b2b017b650e2df
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-16 17:47:27 +0800

    set express-handlebars 6.0.1

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+import { engine } from 'express-handlebars';
+const app = express();
 const port = 3000
 
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set("views", "./views")
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
