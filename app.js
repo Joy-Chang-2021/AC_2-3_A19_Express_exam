19d4f280e2cd50783696f09b95bde41791fd053c
Joy-Chang-2021 <joy50422@gmail.com>
2021-11-16 19:37:19 +0800

    set main.hbs & style.css

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ import { engine } from 'express-handlebars';
 const app = express();
 const port = 3000
 
+app.use(express.static('public'))
+
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set("views", "./views")
+
 
 app.get('/', (req, res) => {
   res.render('index')
